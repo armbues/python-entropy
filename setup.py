9ae9553b3eb05e9e23d2b16240879e77b2af7a74
Martin Natano <natano@natano.net>
2013-10-23 22:26:19 +0200

    fix setup.py
     - bump version number
     - add missing semicolon
     - fix typo

--- setup.py
+++ setup.py
@@ -10,15 +10,15 @@ entropymodule = Extension(
 
 setup(
     name='entropy',
-    version='0.1',
+    version='0.2',
     author='Martin Natano',
     author_email='natano@natano.net',
-    description='extension module for calculation entropy fast',
+    description='extension module for calculating entropy fast',
     url='http://github.com/natano/python-entropy/',
     license='ISC',
     ext_modules=[entropymodule],
     classifiers=[
-        'Intended Audience :: Developers'
+        'Intended Audience :: Developers',
         'License :: OSI Approved :: ISC License (ISCL)',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
