e0ea9d0201c5a1f19a5bc0770604b4bea89ba82c
Martin Natano <natano@natano.net>
2013-10-24 12:13:57 +0200

    add unit tests

---
+++ test_entropy.py
@@ -0,0 +1,26 @@
+from nose.tools import *
+
+from entropy import shannon_entropy
+
+
+class TestShannonEntropy(object):
+    def assert_entropy(self, data, expected):
+        assert_almost_equal(shannon_entropy(data), expected, places=3)
+
+    def test_zero(self):
+        self.assert_entropy('\x00' * 1024, 0.0)
+
+    def test_one(self):
+        self.assert_entropy('\xff' * 1024, 0.0)
+
+    def test_alternate_0f(self):
+        self.assert_entropy('\x00\xff' * 512, 0.125)
+
+    def test_alternate_f0(self):
+        self.assert_entropy('\xff\x00' * 512, 0.125)
+
+    def test_alternate_0cf(self):
+        self.assert_entropy('\x00\xcc\xff' * 512, 0.198)
+
+    def test_one(self):
+        self.assert_entropy(''.join(chr(i) for i in xrange(256)), 1.0)
